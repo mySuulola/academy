126142f9d719c8964a15ca3e17e62759329be3fa
Oluwaseyi Suulola <oluwaseyisuulola@gmail.com>
2020-06-14 04:12:01 -0700

    up and running, home and authenticate screens created with minimal code setup

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+// stle
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('home'),
+      
+    );
+  }
+}
